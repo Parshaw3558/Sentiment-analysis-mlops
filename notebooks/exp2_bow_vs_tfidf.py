7579f543ab41d4da95ee03a10b8293501b517356
parshaw <parshawpatil123@gmail.com>
2026-06-21 12:25:53 +0530

    ready notebook file

--- notebooks/exp2_bow_vs_tfidf.py
+++ notebooks/exp2_bow_vs_tfidf.py
@@ -28,9 +28,9 @@ warnings.filterwarnings("ignore")
 CONFIG = {
     "data_path": "notebooks/data.csv",
     "test_size": 0.2,
-    "mlflow_tracking_uri": "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow",
-    "dagshub_repo_owner": "vikashdas770",
-    "dagshub_repo_name": "YT-Capstone-Project",
+    "mlflow_tracking_uri": "https://dagshub.com/Parshaw3558/sentiment-analysis-mlops.mlflow",
+    "dagshub_repo_owner": "Parshaw3558",
+    "dagshub_repo_name": "sentiment-analysis-mlops",
     "experiment_name": "Bow vs TfIdf"
 }
 
